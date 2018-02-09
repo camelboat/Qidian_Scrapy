c47a1729c51f9a3ca5117792df7aed3c8ac07129
Changxu Luo <camelboat@sjtu.edu.cn>
2018-02-09 12:22:55 +0800

    0.0.1

---
+++ example.jl
@@ -0,0 +1,48 @@
+{"title": ["权限之主"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=400128716\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">小小鱼虾</a>"]}
+{"title": ["来自万界的漂流瓶"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=401746595\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">路天崎</a>"]}
+{"title": ["无耻大魔王"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=400211540\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">雪菜萝卜</a>"]}
+{"title": ["无限创界神"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=4730791\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">南山四鬼</a>"]}
+{"title": ["异界纵横之召唤英雄"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=5096309\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">伯爵与妖精</a>"]}
+{"title": ["天道图库"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=1124686\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">头痛的没法</a>"]}
+{"title": ["神奇黄金指"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=7801966\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">孔长生</a>"]}
+{"title": ["一拳超神"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=401576410\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">浪里来</a>"]}
+{"title": ["网购系统拯救异界"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=2604577\" data-eid=\"qd_F25\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">空空大湿</a>"]}
+{"title": [], "author": []}
+{"title": [], "author": []}
+{"title": [], "author": []}
+{"title": [], "author": []}
+{"title": ["万能复制术"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=10512948\" data-eid=\"qd_F34\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">步飞凡</a>"]}
+{"title": ["万界剥夺器"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=401375077\" data-eid=\"qd_F34\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">追光使者01</a>"]}
+{"title": ["神话入侵异世界"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=401689875\" data-eid=\"qd_F34\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">风尘江南</a>"]}
+{"title": ["超神吞噬"], "author": []}
+{"title": ["始乱武者"], "author": []}
+{"title": ["九州召唤系统"], "author": []}
+{"title": ["超量进化"], "author": []}
+{"title": ["叫我师父大人"], "author": []}
+{"title": ["我在异界送外卖"], "author": []}
+{"title": ["我的系统有点卡"], "author": []}
+{"title": ["进击的骷髅"], "author": []}
+{"title": ["随身手机系统"], "author": []}
+{"title": ["我给系统当奶爸"], "author": []}
+{"title": ["掌运星河"], "author": []}
+{"title": ["我都召唤到了什么"], "author": []}
+{"title": ["世尊"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=8011607\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">夜南听风</a>"]}
+{"title": ["捡了本天书"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=4362725\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">麻烦</a>"]}
+{"title": ["酋长别打脸"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=1019381\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">相思洗红豆</a>"]}
+{"title": ["系统请排队"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=401321562\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">梨花白石</a>"]}
+{"title": ["玄幻百器谱"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=400743545\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">沧月玄</a>"]}
+{"title": ["圣光下的死亡领主"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=400968356\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">香菜芝士</a>"]}
+{"title": ["武念神帝"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=3887914\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">风靡网络</a>"]}
+{"title": ["一剑倾天"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=10503093\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">雁飞鸣</a>"]}
+{"title": ["地球从未毁灭"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=4797843\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">丶浊浪东流</a>"]}
+{"title": ["战道天图"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=11038089\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">矮七辣椒</a>"]}
+{"title": ["超级宠物供应商"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=401190442\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">茕茕肥兔酱</a>"]}
+{"title": ["神国物语"], "author": ["<a class=\"author\" href=\"//me.qidian.com/authorIndex.aspx?id=5212833\" data-eid=\"qd_F43\" target=\"_blank\"><img src=\"//qidian.gtimg.com/qd/images/ico/user.f22d3.png\">极地西瓜</a>"]}
+{"title": ["开挂主播异界纵横"], "author": []}
+{"title": ["全能师尊"], "author": []}
+{"title": ["史上最强书生"], "author": []}
+{"title": ["万界APP"], "author": []}
+{"title": ["至高审判长"], "author": []}
+{"title": ["一拳狂男"], "author": []}
+{"title": ["金牌招仙师"], "author": []}
+{"title": ["神宠小店"], "author": []}
